8335e15d40f90e723e18b5a1cbc72a4745db880b
Kethllen Andrey <kety_andrey@hotmail.com>
2025-08-04 21:39:04 -0300

    init project

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/kethllen/explicaAi/configuration"
+)
+
+func main() {
+	fmt.Println("teste")
+
+	go configuration.NewApplication().Start()
+	shutDown()
+}
+
+func shutDown() {
+	signalShutdown := make(chan os.Signal, 2)
+	signal.Notify(signalShutdown, syscall.SIGINT, syscall.SIGTERM)
+	switch <-signalShutdown {
+	case syscall.SIGINT:
+		fmt.Println("SIGINT signal, explicAI is stopping....")
+	case syscall.SIGTERM:
+		fmt.Println("SIGTERM signal, explicAI is stopping....")
+	}
+}
